390d8d03014084806fd2d8e1eb432fe69342fd99
Guillaume Rousselet <Guillaume.Rousselet@glasgow.ac.uk>
2016-06-01 14:57:26 +0100

    removed list option + help edit

--- bootse.m
+++ bootse.m
@@ -1,19 +1,25 @@
-function bse = bootse(x,nboot,list,est,q)
+function bse = bootse(x,nboot,est,q)
 
-% bse = bootse(x,nboot,list,est,q)
+% bse = bootse(x,nboot,est,q)
 % bootstrap estimate of the standard error of any estimator
-% 
-% See also pbci pb2ig pb2dg 
+%
+% x = vector of observations
+% nboot = number of bootstrap samples, e.g. 1000
+% est = estimator, e.g. 'median'
+% q = quantile for hd estimator
+%
+% See also pbci pb2ig pb2dg hd
 
 % Copyright (C) 2007, 2013 Guillaume Rousselet - University of Glasgow
 
 % GAR, University of Glasgow, Dec 2007
 % GAR, April 2013: replace randsample with randi
+% GAR, 2016-06-01: removed list option, edited help
 
-if nargin<2;nboot=1000;est='hd';list=[];end
-if nargin<5;q=.5;end
+if nargin<2;nboot=1000;est='hd';q=.5;end
 
-n=length(x); % number of subjects
+x = x(:);
+n = numel(x); % number of observations
 boot = zeros(1,nboot);
 if isempty(list)
    list=randi(n,nboot,n); 
